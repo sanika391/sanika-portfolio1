ba245c6b7486baf80e46bb067c32f468c0421912
Sanika <116996971+sanika391@users.noreply.github.com>
2024-10-31 01:21:34 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "SANIKA CHAVAN",
   profile: '/profile.jpg',
-  designation: "Software Developer",
+  designation: "Software Engineer",
   description: "My name is Sanika Chavan. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'sanikachava1806@gmail.com',
   phone: '+917887403910',
